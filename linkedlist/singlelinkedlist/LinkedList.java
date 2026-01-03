6b4a4a5017a2a32602a9392c9318189dc355f409
Rakesh Aitham <siri@Rakeshs-MacBook-Air.local>
2026-01-03 17:11:15 +0530

    linkedlist operations

--- linkedlist/singlelinkedlist/LinkedList.java
+++ linkedlist/singlelinkedlist/LinkedList.java
@@ -1,4 +1,4 @@
-package linkedlist;
+package linkedlist.singlelinkedlist;
 
 class Node {
     int data;
--- linkedlist/singlelinkedlist/LinkedListTest.java
+++ linkedlist/singlelinkedlist/LinkedListTest.java
@@ -1,4 +1,4 @@
-package linkedlist;
+package linkedlist.singlelinkedlist;
 
 public class LinkedListTest {
     public static void main(String[] args) {
